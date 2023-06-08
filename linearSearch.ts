99f8426215ff9d86da2b21307e32cac408644362
emanuelefavero <emafaux89@gmail.com>
2023-06-08 17:11:20 +0200

    add linear search in typescript

---
+++ linearSearch.ts
@@ -0,0 +1,12 @@
+// O(n)
+
+function linearSearch(array: number[], target: number) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) return i
+  }
+
+  return -1
+}
+
+console.log(linearSearch([1, 2, 3, 4], 3)) // 2 (value 3 is at index 2)
+console.log(linearSearch([1, 2, 3, 4], 6)) // -1 (value 6 is not in the array)
