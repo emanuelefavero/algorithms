965cd69f86d2159c72c169b53c5ba6ed19cd3d94
emanuelefavero <emafaux89@gmail.com>
2023-06-08 18:23:20 +0200

    add typescript binary search

---
+++ binarySearch.ts
@@ -0,0 +1,22 @@
+// O(log n) - logarithmic
+
+function binarySearch(array: number[], target: number) {
+  let left = 0
+  let right = array.length - 1
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2)
+
+    if (array[middle] === target) {
+      return middle // found, return index
+    } else if (array[middle] < target) {
+      left = middle + 1
+    } else if (array[middle] > target) {
+      right = middle - 1
+    }
+  }
+  return -1
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5], 4)) // 3
+console.log(binarySearch([1, 2, 3, 4, 5], 6)) // -1
