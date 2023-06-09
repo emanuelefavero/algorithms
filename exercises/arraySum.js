3cf53d896d8ab8742c99e0742016588462119922
emanuelefavero <emafaux89@gmail.com>
2023-06-10 01:24:33 +0200

    add arraySum exercise

---
+++ exercises/arraySum.js
@@ -0,0 +1,18 @@
+function arraySum(array) {
+  return array.reduce((sum, number) => {
+    return sum + number
+  }, 0) // 0 is the initial sum value
+}
+
+function arraySum2(array) {
+  let sum = 0
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i]
+  }
+
+  return sum
+}
+
+console.log(arraySum([1, 2, 3])) // 6
+console.log(arraySum2([1, 2, 3])) // 6
