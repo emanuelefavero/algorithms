8f8ddce2c0ba224a59955e3b6f9a108034857b4b
emanuelefavero <emafaux89@gmail.com>
2023-06-10 06:22:36 +0200

    add bubbleSort

---
+++ bubbleSort.js
@@ -0,0 +1,42 @@
+// O(n ^ 2) - quadratic time complexity
+
+function bubbleSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    // ***
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        ;[array[j], array[j + 1]] = [array[j + 1], array[j]]
+      }
+    }
+  }
+
+  return array
+}
+
+console.log(bubbleSort([3, 5, 1, 2, 4])) // [ 1, 2, 3, 4, 5 ]
+
+// *** we also subtract i from array.length because the swapped elements at the end of the array will be in the correct place and don't need to be checked anymore
+
+// --------------------------
+// 2nd implementation
+function bubbleSort2(array) {
+  let swap = true
+
+  do {
+    swap = false
+
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        let temp = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = temp
+
+        swap = true
+      }
+    }
+  } while (swap)
+
+  return array
+}
+
+console.log(bubbleSort2([3, 5, 1, 2, 4])) // [1, 2, 3, 4, 5]
