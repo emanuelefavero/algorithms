50e921caf6108b4abe0f83302d5cb6996f035f4e
emanuelefavero <emafaux89@gmail.com>
2023-06-14 13:33:30 +0200

    add exercises/intersection

--- exercises/intersection-unique.js
+++ exercises/intersection-unique.js
@@ -7,7 +7,7 @@ function intersection(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     for (let j = 0; j < array2.length; j++) {
       if (array1[i] === array2[j]) {
-        output.add(array1[i])
+        output.add(array1[i]) // In a set you .add instead of .push
         break
       }
     }
