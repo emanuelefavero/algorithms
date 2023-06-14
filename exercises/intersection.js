a0a5b3c0b591335f91fcf2b75fd301a289adec4d
emanuelefavero <emafaux89@gmail.com>
2023-06-14 13:50:17 +0200

    add exercises/intersection with array filter method

---
+++ exercises/intersection.js
@@ -0,0 +1,11 @@
+// Intersection between two arrays - return an array with all the values that occur in both of the arrays
+// O(n)
+
+function intersection(array1, array2) {
+  let set1 = new Set(array1)
+
+  return array2.filter((element) => set1.has(element))
+}
+
+console.log(intersection([4, 2, 5, 3], [4, 1, 3, 6])) // [4, 3]
+console.log(intersection([4, 4, 4, 3], [4, 4, 4, 6])) // [4, 4, 4]
