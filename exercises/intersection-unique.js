674e464470ef2e8f35edd2047766cfeae580e7b7
emanuelefavero <emafaux89@gmail.com>
2023-06-14 13:49:05 +0200

    add exercises/intersection-unique with array filter

--- exercises/intersection-unique.js
+++ exercises/intersection-unique.js
@@ -1,20 +1,13 @@
 // Intersection between two arrays (unique) - return an array with all the values that occur in both of the arrays
-// Worst O(n^2) - Average O(n or n^2) - Best O(n)
+// O(n)
 
 function intersection(array1, array2) {
-  let output = new Set()
+  let set1 = new Set(array1)
 
-  for (let i = 0; i < array1.length; i++) {
-    for (let j = 0; j < array2.length; j++) {
-      if (array1[i] === array2[j]) {
-        output.add(array1[i]) // In a set you .add instead of .push
-        break
-      }
-    }
-  }
+  let output = new Set(array2.filter((element) => set1.has(element)))
 
-  return [...output] // convert set to array before returning
+  return [...output]
 }
 
 console.log(intersection([4, 2, 5, 3], [4, 1, 3, 6])) // [4, 3]
-console.log(intersection([4, 2, 5, 3], [4, 4, 4, 6])) // [4]
+console.log(intersection([4, 4, 4, 3], [4, 4, 4, 6])) // [4]
