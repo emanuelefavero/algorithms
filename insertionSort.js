c1efd55d4fdcf38d4683d871d6323e486e1e8e71
emanuelefavero <emafaux89@gmail.com>
2023-06-14 05:16:05 +0200

    add insertionSort

---
+++ insertionSort.js
@@ -0,0 +1,17 @@
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let current = array[i]
+    let j = i - 1
+
+    while (j >= 0 && array[j] > current) {
+      array[j + 1] = array[j]
+      j--
+    }
+
+    array[j + 1] = current
+  }
+
+  return array
+}
+
+console.log(insertionSort([3, 2, 5, 4, 1]))
