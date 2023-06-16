bf8b4c785c5cc53397f6dad4a225b361dc28f857
emanuelefavero <emafaux89@gmail.com>
2023-06-16 04:37:41 +0200

    add exercises/averageOfEvenNumbers

---
+++ exercises/averageOfEvenNumbers.js
@@ -0,0 +1,17 @@
+function averageOfEvenNumbers(array) {
+  let sum = 0
+  let evenNumbersCount = 0
+
+  array.forEach((number) => {
+    if (number % 2 === 0) {
+      sum += number
+      evenNumbersCount += 1
+    }
+  })
+
+  let average = sum / evenNumbersCount
+
+  return Number(average.toFixed(1))
+}
+
+console.log(averageOfEvenNumbers([1, 2, 3, 4, 2]))
