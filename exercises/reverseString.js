42773fa9cf1e0c87040539bfe0bf1ac9bde46c13
emanuelefavero <emafaux89@gmail.com>
2023-06-18 10:52:11 +0200

    add exercises/reverseString

---
+++ exercises/reverseString.js
@@ -0,0 +1,12 @@
+function reverseString(string) {
+  let reversed = ''
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed += string[i]
+  }
+
+  return reversed
+}
+
+console.log(reverseString('hello')) // olleh
+console.log(reverseString('kayak')) // kayak
