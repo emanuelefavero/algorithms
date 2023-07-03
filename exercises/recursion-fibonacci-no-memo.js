e24ff22b29ce7c5146460da1aa3776b8e4670410
emanuelefavero <emafaux89@gmail.com>
2023-07-03 03:05:08 +0200

    add comments to exercises/recursion-fibonacci-no-memo

--- exercises/recursion-fibonacci-no-memo.js
+++ exercises/recursion-fibonacci-no-memo.js
@@ -1,5 +1,7 @@
 // This is a fibonacci sequence algorithm with recursion that doesn't use memoization so it is really inefficient.
 // It is only shown because it is very simple to write
+
+// O(2^n) - exponential time complexity
 function fibonacci(n) {
   if (n === 0 || n === 1) return n
 
@@ -11,3 +13,6 @@ console.log(fibonacci(6)) // 8
 console.log(fibonacci(3)) // 2
 console.log(fibonacci(2)) // 1
 console.log(fibonacci(1)) // 1
+
+// ! always use memoization with recursion for the fibonacci sequence
+// @see exercises/recursion-fibonacci.js
