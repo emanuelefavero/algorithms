1da59290415662cc9d8f69856db5152b21fbb05d
emanuelefavero <emafaux89@gmail.com>
2023-07-21 13:18:40 +0200

    add array-reverse.js

---
+++ exercises/array-reverse.js
@@ -0,0 +1,13 @@
+// Create a function that reverses an array
+
+function reverse(array) {
+  let reversed = []
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversed.push(array[i])
+  }
+
+  return reversed
+}
+
+console.log(reverse([1, 2, 3, 4])) // [4, 3, 2, 1]
