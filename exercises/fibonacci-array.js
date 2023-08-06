193491248a0c7181b8f74bc3e6605e1d5f33002a
emanuelefavero <emafaux89@gmail.com>
2023-08-07 01:25:37 +0200

    refactor exercises/fibonacci-array

--- exercises/fibonacci-array.js
+++ exercises/fibonacci-array.js
@@ -1,17 +1,13 @@
-function fibonacciArray(n) {
-  if (n === 0) return [0]
-  if (n === 1) return [1]
+// Generate an array with the fibonacci sequence up to n
+// O(n)
+function fibonacci(n) {
+  let sequence = [0, 1]
 
-  const sequence = [0, 1]
-
-  // TIP: i = 2 is the sequence array length (we start from 2 because we have already assigned the first two numbers 0 and 1 to the array)
   for (let i = 2; i <= n; i++) {
-    const current = sequence[i - 1] + sequence[i - 2]
-
-    sequence.push(current)
+    sequence.push(sequence[i - 2] + sequence[i - 1])
   }
 
   return sequence
 }
 
-console.log(fibonacciArray(10)) // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+console.log(fibonacci(8)) // [ 0, 1, 1, 2, 3, 5, 8, 13, 21 ]
