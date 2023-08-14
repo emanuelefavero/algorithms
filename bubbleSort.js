a4a534909d06f511250dad223d44a301c9a1e912
emanuelefavero <emafaux89@gmail.com>
2023-08-14 15:44:30 +0200

    add exercises/bubbleSort3.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -13,27 +13,3 @@ function bubbleSort(array) {
 }
 
 console.log(bubbleSort([3, 5, 1, 2, 4])) // [ 1, 2, 3, 4, 5 ]
-
-// --------------------------
-// 2nd implementation
-function bubbleSort2(array) {
-  let swap = true
-
-  do {
-    swap = false
-
-    for (let i = 0; i < array.length - 1; i++) {
-      if (array[i] > array[i + 1]) {
-        let temp = array[i]
-        array[i] = array[i + 1]
-        array[i + 1] = temp
-
-        swap = true
-      }
-    }
-  } while (swap)
-
-  return array
-}
-
-console.log(bubbleSort2([3, 5, 1, 2, 4])) // [1, 2, 3, 4, 5]
