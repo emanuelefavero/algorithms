3a212555f4b10c55caa4b3e24c6c74569ccbde0c
emanuelefavero <emafaux89@gmail.com>
2023-08-28 20:42:12 +0200

    add improved algorithm to exercises/is-prime

--- exercises/is-prime.js
+++ exercises/is-prime.js
@@ -1,7 +1,7 @@
 // TIP: A prime number (or a prime) is a natural number greater than 1 that is not a product of two smaller natural numbers
 // Write a function that takes a number as an argument and returns true if the number is a prime number and false if it is not. Do not use recursion
 function isPrime(n) {
-  if (n <= 1) return false
+  if (n < 2) return false
 
   for (let i = 2; i < n; i++) {
     // If n is divisible by i, then n is not prime
@@ -12,6 +12,25 @@ function isPrime(n) {
 }
 
 console.log(isPrime(1)) // false
-console.log(isPrime(2)) // true
 console.log(isPrime(11)) // true
 console.log(isPrime(33)) // false
+
+// -------------
+// isPrime - improved
+// We could improve the function by only checking up to the square root of n
+// If n is divisible by a number greater than its square root, then it is also divisible by a number smaller than it
+function isPrime2(n) {
+  if (n < 2) return false
+
+  let squareRoot = Math.sqrt(n)
+
+  for (let i = 2; i < squareRoot; i++) {
+    if (n % i === 0) return false
+  }
+
+  return true
+}
+
+console.log(isPrime2(1)) // false
+console.log(isPrime2(11)) // true
+console.log(isPrime2(33)) // false
