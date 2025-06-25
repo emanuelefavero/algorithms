e1031a5d04501481b8ebe27fe3d8e9e2d4b7b9cc
Emanuele Favero <emafaux89@gmail.com>
2025-06-25 20:28:06 +0200

    refactor: fibonacci-print

--- exercises/fibonacci-print.js
+++ exercises/fibonacci-print.js
@@ -1,10 +1,10 @@
 // Create a function that prints all the numbers of the Fibonacci sequence up to a given number
 function fib(n) {
-  console.log(0)
-  console.log(1)
-
   let previous = 0
   let current = 1
+
+  console.log(previous)
+  console.log(current)
 
   for (let i = 2; i <= n; i++) {
     let next = previous + current
